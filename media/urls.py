f408d143841de42ec25ef4a84be644371a6b8297
robogeeko <robogeeko@gmail.com>
2025-12-25 08:42:46 -0500

    Adding navbar and basic library view

--- media/urls.py
+++ media/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from media import api
+from media import api, views
 
 app_name = "media"
 
 urlpatterns = [
+    path("library/", views.library_view, name="library"),
     path("api/media/status/", api.get_media_status, name="get_media_status"),
     path("api/media/wanted/", api.add_wanted_media, name="add_wanted_media"),
 ]
--- media/views.py
+++ media/views.py
@@ -1,2 +1,19 @@
+from __future__ import annotations
 
-# Create your views here.
+from django.shortcuts import render
+
+from media.models import Audiobook, Book
+
+
+def library_view(request):
+    books = Book.objects.all().order_by("-added_date")[:50]
+    audiobooks = Audiobook.objects.all().order_by("-added_date")[:50]
+
+    context = {
+        "books": books,
+        "audiobooks": audiobooks,
+        "total_books": Book.objects.count(),
+        "total_audiobooks": Audiobook.objects.count(),
+    }
+
+    return render(request, "media/library.html", context)
